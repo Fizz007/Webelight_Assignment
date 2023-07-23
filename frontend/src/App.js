41e47e8c41c26962e99b2186a2dfbfa55aac017f
Faisal Abrar <erfaisalabrar@gmail.com>
2023-07-23 22:33:23 +0530

    Slider updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/"  element={
               <>
-                {/* <Slider /> */}
+                <Slider />
                 <Home />
               </>
             }
